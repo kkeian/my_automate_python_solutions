b76a7aa4b305fcabdc88cdb7b8f3a011bff0ae2d
Keian Kaserman <kaserman22@gmail.com>
2019-09-15 15:49:05 -0400

    Comma Code chapter 4

---
+++ comma_code.py
@@ -0,0 +1,6 @@
+def returns_sentence_of_list(list_to_change):
+    for i in range(len(list_to_change)):
+        if i != len(list_to_change) - 1:
+            print(list_to_change[0 + i] + ',', end =' ')
+        else:
+            print('and ' + list_to_change[-1])
