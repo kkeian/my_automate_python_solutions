214690cdcb586aaeca69bb761d82d5dc39a65e9c
Keian Kaserman <kaserman22@gmail.com>
2019-09-15 17:31:00 -0400

    remove collatz_sequence, comma_code, and character_picture_grid

--- comma_code.py
+++
@@ -1,8 +0,0 @@
-# This code creates a comma separated sentence out of a list.
-
-def returns_sentence_of_list(list_to_change):
-    for i in range(len(list_to_change)):
-        if i != len(list_to_change) - 1:
-            print(list_to_change[0 + i] + ',', end =' ')
-        else:
-            print('and ' + list_to_change[-1])
