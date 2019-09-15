1ab0e094c7b50c53804b724c0052c7420bd563c6
Keian Kaserman <kaserman22@gmail.com>
2019-09-15 15:56:20 -0400

    Update to comma_code.py

--- comma_code.py
+++ comma_code.py
@@ -1,3 +1,5 @@
+# This code creates a comma separated sentence out of a list.
+
 def returns_sentence_of_list(list_to_change):
     for i in range(len(list_to_change)):
         if i != len(list_to_change) - 1:
